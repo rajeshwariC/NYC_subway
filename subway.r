bc1f1c3b96e1eb60adc25e420452db7065f9db07
rajeshwariC <rajeshwarichaudhari09@gmail.com>
2017-05-03 15:09:34 +0530

    Update subway.r

--- subway.r
+++ subway.r
@@ -9,6 +9,7 @@ table(subway$conds)
 table(subway$ENTRIESn_hourly,subway$rain)
 
 #section 1
+#Statisticalo test
 by(subway$ENTRIESn_hourly,subway$rain,summary)
 will<-wilcox.test(formula=ENTRIESn_hourly~rain,data=subway)
 par(mfrow=c(1,2))
@@ -19,7 +20,9 @@ head(non_rainy_days)
 
 h1<-hist(subway$ENTRIESn_hourly[subway$rain==0],col="red")
 h2<-hist(subway$ENTRIESn_hourly[subway$rain==1],col="blue")
+
 #section2
+#Linear Regression
 model1<-lm(ENTRIESn_hourly~UNIT+hour+weekday+rain+tempi+fog,data=subway)
 summary(model1)
 library(ggplot2)
@@ -29,7 +32,9 @@ attach(rainy_days)
 ggplot(subway,aes(x=ENTRIESn_hourly,fill=rain))+
   geom_histogram(binwidth=0.05,position="dodge")+
   labs(x="hours", y="rain", title="histogram")
-#3
+
+#section3
+#Visualization
 range(subway$ENTRIESn_hourly)
 
 ggplot(aes(x=ENTRIESn_hourly,fill=rain),data=subway)+
